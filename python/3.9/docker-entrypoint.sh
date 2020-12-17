c0e89de39484c58e41d566af2c5593fdbca6bfcf
Fabien D <fabien.dosse@lengow.com>
2020-12-17 15:11:01 +0100

    Add Python docker image files

---
+++ python/3.9/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd /var/www
+
+if [ -f "env/bin/activate" ];
+then
+    echo "Virtualenv found, use it now."
+    source env/bin/activate
+fi
+
+echo 'export PS1="|> 🐳 <| \[\033[1;36m\]\h \[\033[1;34m\]\W\[\033[0;35m\] \[\033[1;36m\]# \[\033[0m\]"' >> .bashrc
+
+exec "$@"
