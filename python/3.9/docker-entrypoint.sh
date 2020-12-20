3145a6f702673fc4f7c2c9d512f637dc618483e4
Fabien D <fabien@myprod.net>
2020-12-20 14:20:38 +0100

    Change custom prompt definition to avoid override local .bashrc

--- python/3.9/docker-entrypoint.sh
+++ python/3.9/docker-entrypoint.sh
@@ -9,6 +9,4 @@ then
     source env/bin/activate
 fi
 
-echo 'export PS1="| 🐍 ${PYTHON_VERSION} | \[\033[1;36m\]\h \[\033[1;34m\]\W\[\033[0;35m\] \[\033[1;36m\]# \[\033[0m\]"' >> .bashrc
-
 exec "$@"
