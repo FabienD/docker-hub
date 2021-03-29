0a3965b41aecc293c124da20dbe04782e6c4abc9
Fabien D <fabien.dosse@lengow.com>
2021-03-29 18:06:30 +0200

    Create env variable from secret for PGP keys

--- python/3.9/docker-entrypoint.sh
+++ python/3.9/docker-entrypoint.sh
@@ -9,4 +9,14 @@ then
     source venv/bin/activate
 fi
 
+if [ -f "/run/secrets/pgp_public_key" ];
+then
+    export PGP_PUBLIC_KEY=`cat /run/secrets/pgp_public_key`
+fi
+
+if [ -f "/run/secrets/pgp_private_key" ];
+then
+    export PGP_PRIVATE_KEY=`cat /run/secrets/pgp_private_key`
+fi
+
 exec "$@"
